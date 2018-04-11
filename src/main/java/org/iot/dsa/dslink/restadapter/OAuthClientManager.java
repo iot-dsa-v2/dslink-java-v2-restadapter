b3ac61142b6aa66f3889efc24820a0817db9858d
JurisPuchin <juris.puchin@gmail.com>
2018-04-11 14:33:51 -0700

    Added OAuth capability.

---
+++ src/main/java/org/iot/dsa/dslink/restadapter/OAuthClientManager.java
@@ -0,0 +1,109 @@
+package org.iot.dsa.dslink.restadapter;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.rs.security.oauth2.client.Consumer;
+import org.apache.cxf.rs.security.oauth2.client.OAuthClientUtils;
+import org.apache.cxf.rs.security.oauth2.common.AccessTokenGrant;
+import org.apache.cxf.rs.security.oauth2.common.ClientAccessToken;
+import org.apache.cxf.rs.security.oauth2.grants.clientcred.ClientCredentialsGrant;
+import org.apache.cxf.rs.security.oauth2.grants.owner.ResourceOwnerGrant;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthServiceException;
+import org.iot.dsa.util.DSException;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author James (Juris) Puchin
+ * Created on 4/10/2018
+ */
+public class OAuthClientManager {
+    private WebClient accessTokenService;
+    private String userName;
+    private String password;
+    private long expireAt;
+    private ClientAccessToken token;
+    private final Object tokenLock;
+    private Consumer consumer;
+
+    /**
+     * Manage OAuth2 for Client flow.
+     */
+    OAuthClientManager(String clientID, String clientSecret, String tokenURL) {
+        this(clientID, clientSecret, null, null, tokenURL);
+    }
+
+    /**
+     * Manage Oauth2 for Password flow.
+     */
+    OAuthClientManager(String clientID, String clientSecret, String user, String password, String tokenURL) {
+        this.userName = user;
+        this.password = password;
+        consumer = new Consumer(clientID, clientSecret);
+        accessTokenService = WebClient.create(tokenURL);
+        tokenLock = new Object();
+    }
+
+    public ClientAccessToken getAccessToken() {
+        synchronized (tokenLock) {
+            if (token == null) {
+                getNewToken();
+            } else if (expireAt > 0 && System.currentTimeMillis() >= expireAt) {         //Refresh if expired
+                refreshToken();
+            }
+        }
+        return token;
+    }
+
+    private void getNewToken() {
+        AccessTokenGrant grant;
+
+        if (password != null || userName != null) {
+            grant = new ResourceOwnerGrant(userName, password);
+        } else {
+            grant = new ClientCredentialsGrant();
+        }
+
+        try {
+            token = OAuthClientUtils.getAccessToken(accessTokenService, consumer, grant);
+            setExpirationTime();
+        } catch (OAuthServiceException ex) {
+            DSException.throwRuntime(ex);
+        }
+    }
+
+    private void refreshToken() {
+        try {
+            token = OAuthClientUtils.refreshAccessToken(accessTokenService, consumer, token);
+            setExpirationTime();
+        } catch (OAuthServiceException ex) {
+            getNewToken();
+        }
+    }
+
+    /**
+     * Force token refresh or re-acquisition.
+     */
+    public void onAuthFailed() {
+        synchronized (tokenLock) {
+            String refreshToken = token.getRefreshToken();
+            if (refreshToken != null) {
+                refreshToken();
+            } else {
+                getNewToken();
+            }
+        }
+    }
+
+    private void setExpirationTime() {
+        token.setIssuedAt(System.currentTimeMillis());
+        token.setExpiresIn(TimeUnit.MILLISECONDS.convert(token.getExpiresIn(), TimeUnit.SECONDS));
+
+        if (token.getExpiresIn() > 0) {
+            expireAt = token.getIssuedAt() + token.getExpiresIn();
+        }
+    }
+
+    public String createAuthorizationHeader() {
+        return OAuthClientUtils.createAuthorizationHeader(getAccessToken());
+    }
+}
--- src/main/java/org/iot/dsa/dslink/restadapter/WebClientProxy.java
+++ src/main/java/org/iot/dsa/dslink/restadapter/WebClientProxy.java
@@ -1,89 +1,105 @@
 package org.iot.dsa.dslink.restadapter;
 
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.apache.cxf.jaxrs.client.WebClient;
-import org.iot.dsa.DSRuntime;
+import org.apache.cxf.rs.security.oauth2.client.OAuthClientUtils;
+import org.apache.cxf.rs.security.oauth2.common.ClientAccessToken;
 import org.iot.dsa.node.DSMap;
 import org.iot.dsa.node.DSMap.Entry;
 import org.iot.dsa.util.DSException;
+
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.function.Consumer;
 
 public class WebClientProxy {
 
     private String username;
     private String password;
-    private String clientID;
-    private String clientSecret;
-    private String tokenURL;
     private Util.AUTH_SCHEME scheme;
+    private OAuthClientManager authManager;
 
-    private WebClientProxy(String username, String password, String clientID, String clientSecret, String tokenURL, Util.AUTH_SCHEME scheme) {
+    private WebClientProxy(String username, String password, Util.AUTH_SCHEME scheme, OAuthClientManager authMngr) {
         this.username = username;
         this.password = password;
-        this.clientID = clientID;
-        this.clientSecret = clientSecret;
-        this.tokenURL = tokenURL;
         this.scheme = scheme;
+        this.authManager = authMngr;
     }
 
     public static WebClientProxy buildNoAuthClient() {
-        return new WebClientProxy(null, null, null, null, null, Util.AUTH_SCHEME.NO_AUTH);
+        return new WebClientProxy(null, null, Util.AUTH_SCHEME.NO_AUTH, null);
     }
-    
+
     public static WebClientProxy buildBasicUserPassClient(String username, String password) {
-        return new WebClientProxy(username, password, null, null, null, Util.AUTH_SCHEME.BAISC_USR_PASS);
+        return new WebClientProxy(username, password, Util.AUTH_SCHEME.BAISC_USR_PASS, null);
     }
 
     public static WebClientProxy buildClientFlowOAuth2Client(String clientID, String clientSecret, String tokenURL) {
-        return new WebClientProxy(null, null, clientID, clientSecret, tokenURL, Util.AUTH_SCHEME.OAUTH2_CLIENT);
+        OAuthClientManager mngr = new OAuthClientManager(clientID, clientSecret, tokenURL);
+        return new WebClientProxy(null, null, Util.AUTH_SCHEME.OAUTH2_CLIENT, mngr);
     }
 
     public static WebClientProxy buildPasswordFlowOAuth2Client(String username, String password, String clientID, String clientSecret, String tokenURL) {
-        return new WebClientProxy(username, password, clientID, clientSecret, tokenURL, Util.AUTH_SCHEME.OAUTH2_USR_PASS);
+        OAuthClientManager mngr = new OAuthClientManager(clientID, clientSecret, username, password, tokenURL);
+        return new WebClientProxy(username, password, Util.AUTH_SCHEME.OAUTH2_USR_PASS, mngr);
     }
-	
-	public Response get(String address, DSMap urlParameters) {
-		WebClient client = prepareWebClient(address, urlParameters);
-		Response r = client.get();
-		client.close();
-		return r;
-	}
-	
-	public Response put(String address, DSMap urlParameters, Object body) {
-	    WebClient client = prepareWebClient(address, urlParameters);
-	    Response r = client.put(body);
-	    client.close();
-	    return r;
-	}
-	
-	public Response post(String address, DSMap urlParameters, Object body) {
+
+//    public Response get(String address, DSMap urlParameters) {
+//        WebClient client = prepareWebClient(address, urlParameters);
+//        Response r = client.get();
+//        client.close();
+//        return r;
+//    }
+//
+//    public Response put(String address, DSMap urlParameters, Object body) {
+//        WebClient client = prepareWebClient(address, urlParameters);
+//        Response r = client.put(body);
+//        client.close();
+//        return r;
+//    }
+//
+//    public Response post(String address, DSMap urlParameters, Object body) {
+//        WebClient client = prepareWebClient(address, urlParameters);
+//        Response r = client.post(body);
+//        client.close();
+//        return r;
+//    }
+//
+//    public Response delete(String address, DSMap urlParameters) {
+//        WebClient client = prepareWebClient(address, urlParameters);
+//        Response r = client.delete();
+//        client.close();
+//        return r;
+//    }
+//
+//    public Response patch(String address, DSMap urlParameters, Object body) {
+//        return invoke("PATCH", address, urlParameters, body);
+//    }
+
+    public Response invoke(String httpMethod, String address, DSMap urlParameters, Object body) {
         WebClient client = prepareWebClient(address, urlParameters);
-        Response r = client.post(body);
-        client.close();
+        if (authManager != null)
+            client.header(HttpHeaders.AUTHORIZATION, authManager.createAuthorizationHeader());
+        Response r = null;
+        try {
+            r = client.invoke(httpMethod, body);
+        } catch (NotAuthorizedException ex) {
+            if (authManager != null) {
+                authManager.onAuthFailed();
+                client.replaceHeader(HttpHeaders.AUTHORIZATION, authManager.createAuthorizationHeader());
+                r = client.invoke(httpMethod, body);
+            } else {
+                DSException.throwRuntime(ex);
+            }
+        } finally {
+            client.close();
+        }
         return r;
     }
-	
-	public Response delete(String address, DSMap urlParameters) {
-        WebClient client = prepareWebClient(address, urlParameters);
-        Response r = client.delete();
-        client.close();
-        return r;
-    }
-	
-	public Response patch(String address, DSMap urlParameters, Object body) {
-        return invoke("PATCH", address, urlParameters, body);
-    }
-	
-	public Response invoke(String httpMethod, String address, DSMap urlParameters, Object body) {
-	    WebClient client = prepareWebClient(address, urlParameters);
-        Response r = client.invoke(httpMethod, body);
-        client.close();
-        return r;
-	}
-	
-	private WebClient prepareWebClient(String address, DSMap urlParameters) {
-	    WebClient client = configureAuthorization(address);
+
+    private WebClient prepareWebClient(String address, DSMap urlParameters) {
+        WebClient client = configureAuthorization(address);
         client.accept(MediaType.APPLICATION_JSON);
         for (int i = 0; i < urlParameters.size(); i++) {
             Entry entry = urlParameters.getEntry(i);
@@ -91,18 +107,22 @@ public class WebClientProxy {
             client.query(entry.getKey(), value);
         }
         return client;
-	}
+    }
 
-	private WebClient configureAuthorization(String address) {
+    private WebClient configureAuthorization(String address) {
         WebClient client = null;
         switch (scheme) {
             case NO_AUTH:
-                client =  WebClient.create(address);
+                client = WebClient.create(address);
                 break;
             case BAISC_USR_PASS:
                 client = WebClient.create(address, username, password, null);
                 break;
             case OAUTH2_CLIENT:
+                client = WebClient.create(address);
+                ClientAccessToken token = authManager.getAccessToken();
+                client.header(HttpHeaders.AUTHORIZATION, OAuthClientUtils.createAuthorizationHeader(token));
+                break;
             case OAUTH2_USR_PASS:
                 DSException.throwRuntime(new RuntimeException("Unsupported authorization type!"));
                 break;
